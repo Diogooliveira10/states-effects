39b739582dc66cea014e9e28c7a1e9e8b9a62189
ANJOS <ddiogooliveira9@gmail.com>
2022-02-02 13:04:04 -0300

    Criando componente utilizando estados

--- aula-hooks/src/Components/App/App.js
+++ aula-hooks/src/Components/App/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Counter from '../Counter/Counter';
+import Smartcount from '../Smartcounter/Smartcounter';
+
 
 function App() {
   return (
     <>
       <h1>Hello World!</h1>
-      <Counter/>
+      <Smartcount/>
     </>
   );
 }
