4ee9c605cf7fd12a3eaa7f758350f34326b6b14d
ANJOS <ddiogooliveira9@gmail.com>
2022-02-02 15:09:09 -0300

    Criando um componente prático com estados

--- aula-hooks/src/Components/App/App.js
+++ aula-hooks/src/Components/App/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import Counter from '../Counter/Counter';
 import Smartcount from '../Smartcounter/Smartcounter';
+import Ifoodcounter from '../Ifoodcounter/Ifoodcounter';
 
 
 function App() {
   return (
     <>
-      <h1>Hello World!</h1>
-      <Smartcount/>
+      <h1>Ifood</h1>
+      <Ifoodcounter/>
     </>
   );
 }
